a2d9e316bcf6bdbf16b5991a5b49ec49322bda60
vaibhavkhadilkar <vvk072000@utdallas.edu>
2010-12-15 14:54:38 -0600

    deleted packages

--- src/main/java/com/talis/hbase/rdf/HBaseRdfFactory.java
+++
@@ -1,9 +0,0 @@
-package com.talis.hbase.rdf;
-
-public class HBaseRdfFactory {
-
-    public static DatasetGraphHBase createDatasetGraph(LocationHBase location) { 
-    	return HBaseRdfMaker._createDatasetGraph(location) ; 
-    }
-	
-}
