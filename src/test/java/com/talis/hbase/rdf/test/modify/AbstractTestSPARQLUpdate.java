af25ec121f12e232a7231b1588badeea19e5e902
vaibhavkhadilkar <vvk072000@utdallas.edu>
2011-05-19 14:08:44 -0500

    updating test folder structure

--- src/test/java/com/talis/hbase/rdf/test/modify/AbstractTestSPARQLUpdate.java
+++
@@ -1,8 +0,0 @@
-package com.talis.hbase.rdf.test.modify;
-
-import com.hp.hpl.jena.sparql.modify.TestUpdateGraph;
-
-public abstract class AbstractTestSPARQLUpdate extends TestUpdateGraph
-{
-	//List tests that fail here
-}
